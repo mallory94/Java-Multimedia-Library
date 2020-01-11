1d0893303a4dc6a737e6eb98f8558c67af1bd17f
mallory94 <malloga@hotmail.fr>
2020-01-11 22:10:30 +0100

    suppression de la fonction lireDansSocket() présente dans Service

--- PartieServeur/src/services/Service.java
+++ PartieServeur/src/services/Service.java
@@ -34,33 +34,5 @@ public abstract class Service {
 	
 	protected Socket getSocket() {
 		return this.client;
-	}
-	
-	//tentative de factorisation d'une fonction qui lit un nombre dans la socket
-	//A SUPPRIMER si la tentative echoue
-	public int lireIntDansSocket() {
-		Integer numeroLu = null;
-		try {
-			BufferedReader in = new BufferedReader(
-				new InputStreamReader(this.getSocket().getInputStream())
-				);
-			String reponse = in.readLine();
-			try {
-				 numeroLu = Integer.valueOf(reponse);
-			}
-			catch (NumberFormatException e) {
-				e.printStackTrace();
-			}
-			if (numeroLu != null) {
-				return(numeroLu);
-			}
-		}
-		catch (IOException e) {
-			System.out.println("Probleme dans la lecture de la socket");
-		}
-		
-		return (Integer) null; //empeche l'affichage d'une erreur de return
-	}
-		
-		
+	}	
 }
